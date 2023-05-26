dfec7aacc5db8b98c74db0a7848bd87877853e0e
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-05-26 17:44:19 +0900

    a

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,7 +1,14 @@
 #include "Enemy.h"
 #include <assert.h>
 #include "Matrix.h"
-void Enemy::Initialize(Model* model, const Vector3& position, const Vector3& velocity) { 
+
+Enemy::Enemy() { 
+	state = new EnemyStateApproach(); 
+}
+Enemy::~Enemy() { 
+	//delete state;
+}
+void Enemy::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
 	assert(model);
 	textureHandle_ = TextureManager::Load("Angel.png");
 	model_ = model;
@@ -9,44 +16,47 @@ void Enemy::Initialize(Model* model, const Vector3& position, const Vector3& vel
 	worldTransform_.translation_ = position;
 	velocity_ = velocity;
 }
-void Enemy::Update() {/*
-	switch (phase_) {
-	case Phase::Approach:
-	default:
-		worldTransform_.UpdateMatrix();
-		velocity_ = {0, 0, -0.5f};
-		velocity_ = TransformNormal(velocity_, worldTransform_.matWorld_);
-		worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
-		if (worldTransform_.translation_.z < -10.0f) {
-			phase_ = Phase::Leave;
-		}
-		break;
-	case Phase::Leave:
-		worldTransform_.UpdateMatrix();
-		velocity_ = {-0.25f, 0.25f, -0.25f};
-		velocity_ = TransformNormal(velocity_, worldTransform_.matWorld_);
-		worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
-		break;
-	}*/
-	(this->*spFuncTable[static_cast<size_t>(phase_)])();
+void Enemy::Update() {
+	state->Update(this);
 	worldTransform_.UpdateMatrix();
-	velocity_ = TransformNormal(velocity_, worldTransform_.matWorld_);
+	worldTransform_.TransferMatrix();
+	/*velocity_ = TransformNormal(velocity_, worldTransform_.matWorld_);*/
 }
 void Enemy::Draw(const ViewProjection& viewProjection) {
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
 }
-void Enemy::Approach() {
-	velocity_ = {0, 0, -0.25f};
-	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
-	if (worldTransform_.translation_.z < -10.0f) {
-		phase_ = Phase::Leave;
+void Enemy::move(const Vector3& velocity) {
+	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity);
+}
+//void Enemy::Approach() { 
+//	move({0.0f, 0.0f, -0.5f});
+//	if (worldTransform_.translation_.z < -10.0f) {
+//		ChangeState(new EnemyStateLeave());
+//	}
+//}
+//void Enemy::Leave() { 
+//	move({-0.25f, 0.25f, 0.0f});
+//}
+void Enemy::ChangeState(BaseEnemyState* newState) { 
+	delete state;
+	state=newState;
+}
+
+//void BaseEnemyState::Update(Enemy* enemy) {
+//
+//}
+void EnemyStateApproach::Update(Enemy* enemy) {
+	const Vector3 velocity = {0.0f, 0.0f, -0.5f};
+	enemy->move(velocity);
+	if (enemy->GetTransform().translation_.z < -10.0f) {
+		enemy->ChangeState(new EnemyStateLeave());
 	}
 }
-void Enemy::Leave() {
-	velocity_ = {-0.25f, 0.25f, -0.25f};
-	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
+void EnemyStateLeave::Update(Enemy* enemy) {
+	const Vector3 velocity = {-0.25f, 0.25f, -0.25f};
+	enemy->move(velocity);
 }
-void (Enemy::*Enemy::spFuncTable[])() = {
-	&Enemy::Approach,
-	&Enemy::Leave
-};
+//void (Enemy::*Enemy::spFuncTable[])() = {
+//	&Enemy::Approach,
+//	&Enemy::Leave
+//};
--- Enemy.h
+++ Enemy.h
@@ -2,23 +2,50 @@
 #include <Model.h>
 #include <WorldTransform.h>
 
+class Enemy;
+
+class BaseEnemyState {
+public:
+	virtual void Update(Enemy* enemy) = 0;
+};
+
+class EnemyStateApproach : public BaseEnemyState {
+public:
+	void Update(Enemy* enemy);
+};
+
+class EnemyStateLeave : public BaseEnemyState {
+public:
+	void Update(Enemy* enemy);
+};
+
 class Enemy {
 public:
+	Enemy();
+	~Enemy();
+
 	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
 	void Update();
 	void Draw(const ViewProjection& viewProjection);
-	void Approach();
-	void Leave();
-	enum class Phase {
-		Approach,
-		Leave,
-	};
+
+	/*void Approach();
+	void Leave();*/
+	void ChangeState(BaseEnemyState* newState);
+	void move(const Vector3& velocity);
+
+	/*enum class Phase {
+	    Approach,
+	    Leave,
+	};*/
+	Vector3 SetPosition(const Vector3& position) { return position; }
+	Vector3 SetVelocity(const Vector3& velocity) { return velocity; }
+	WorldTransform GetTransform() { return worldTransform_; }
 
 private:
 	WorldTransform worldTransform_;
 	Model* model_;
 	uint32_t textureHandle_ = 0u;
 	Vector3 velocity_;
-	Phase phase_;
-	static void (Enemy::*spFuncTable[])();
+
+	BaseEnemyState* state;
 };
